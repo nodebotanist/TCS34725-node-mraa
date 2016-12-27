03966841121e263942fa1092f183883a6a2fdcbc
Kas Perch <the@nodebotani.st>
2016-12-26 19:00:17 -0600

    Added enable sequence

--- index.js
+++ index.js
@@ -6,6 +6,20 @@ const colorSensor = new mraa.I2c(1)
 
 colorSensor.address(0x29)
 
+// check ID register to make sure sensor is plugged in
 let idRegValue = colorSensor.readReg(0x92)
 
-console.log(idRegValue)
+if (idRegValue !== 0x44) {
+    throw new Error("Cannot find TCS34725!")
+}
+
+// set integration time -- register 0x81, value 0xEB
+colorSensor.writeReg(0x81, 0xEB)
+
+// set sensor gain -- register 0x8F, value 0x00
+colorSensor.writeReg(0x8F, 0x00)
+
+// enable sensor -- register 0x80, value 0x01
+// followed by register 0x80, value 0x03
+colorSensor.writeReg(0x80, 0x01)
+colorSensor.writeReg(0x80, 0x03)
