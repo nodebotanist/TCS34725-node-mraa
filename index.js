a85fc7b74275dbf7e75650482222ef8f322c5a0b
Kas Perch <the@nodebotani.st>
2017-01-09 20:44:21 -0600

    Fixed addresses

--- index.js
+++ index.js
@@ -57,25 +57,25 @@ let blueGraph = new Barcli({
 setInterval(() => {
     // start sampling
     // clear channel-- low byte from 0x14, high from 0x15
-    clear = colorSensor.readReg(0x94) << 4
-    clear |= colorSensor.readReg(0x95)
+    clear = colorSensor.readReg(0x95) << 4
+    clear |= colorSensor.readReg(0x94)
     clearGraph.update(clear)
 
     // red channel-- low byte 0x16, high byte 0x17
-    red = colorSensor.readReg(0x96) << 4
-    red |= colorSensor.readReg(0x97)
+    red = colorSensor.readReg(0x97) << 4
+    red |= colorSensor.readReg(0x96)
     red = Math.floor((red / clear) * 256)
     redGraph.update(red)
 
     // green channel-- low byte 0x18, high byte 0x19
-    green = colorSensor.readReg(0x18) << 4
-    green |= colorSensor.readReg(0x19)
+    green = colorSensor.readReg(0x89) << 4
+    green |= colorSensor.readReg(0x88)
     green = Math.floor((green / clear) * 256)
     greenGraph.update(green)
 
     // blue channel-- low byte 0x1A, high byte 0x1B
-    blue = colorSensor.readReg(0x1A) << 4
-    blue |= colorSensor.readReg(0x1B)
+    blue = colorSensor.readReg(0x8B) << 4
+    blue |= colorSensor.readReg(0x8A)
     blue = Math.floor((blue / clear) * 256)
     blueGraph.update(blue)
 }, 250)
