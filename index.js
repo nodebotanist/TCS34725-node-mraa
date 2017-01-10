f35bc4a089f8e3399725cf763415627b87b1c7e4
Kas Perch <the@nodebotani.st>
2017-01-09 21:01:54 -0600

    Debugging

--- index.js
+++ index.js
@@ -57,26 +57,26 @@ let clear, red, green, blue
 setInterval(() => {
     // start sampling
     // clear channel-- low byte from 0x14, high from 0x15
-    clear = colorSensor.readReg(0x95) << 8
-    clear |= colorSensor.readReg(0x94)
+    // clear = colorSensor.readReg(0x95) << 8
+    clear = colorSensor.readReg(0x94)
         //clearGraph.update(clear)
 
     // red channel-- low byte 0x16, high byte 0x17
-    red = colorSensor.readReg(0x97) << 8
-    red |= colorSensor.readReg(0x96)
-    red = Math.floor((red * 255) / clear)
+    // red = colorSensor.readReg(0x97) << 8
+    red = colorSensor.readReg(0x96)
+        //red = Math.floor((red * 255) / clear)
         //redGraph.update(red)
 
     // green channel-- low byte 0x18, high byte 0x19
-    green = colorSensor.readReg(0x99) << 8
-    green |= colorSensor.readReg(0x98)
-    green = Math.floor((green * 255) / clear)
+    // green = colorSensor.readReg(0x99) << 8
+    green = colorSensor.readReg(0x98)
+        //green = Math.floor((green * 255) / clear)
         //greenGraph.update(green)
 
     // blue channel-- low byte 0x1A, high byte 0x1B
-    blue = colorSensor.readReg(0x9B) << 8
-    blue |= colorSensor.readReg(0x9A)
-    blue = Math.floor((blue * 255) / clear)
+    //blue = colorSensor.readReg(0x9B) << 8
+    blue = colorSensor.readReg(0x9A)
+        // blue = Math.floor((blue * 255) / clear)
         //blueGraph.update(blue)
     console.log(clear, red, green, blue)
 }, 250)
