5cf7a534ddce229af93521d2c3ce67eb26444a5e
Kas Perch <the@nodebotani.st>
2017-01-09 21:38:02 -0600

    Debugging

--- index.js
+++ index.js
@@ -39,7 +39,7 @@ async.until(
 let clear, red, green, blue
 let clearGraph = new Barcli({
     label: 'clear',
-    range: [0, 65335]
+    range: [0, 255]
 })
 let redGraph = new Barcli({
     label: 'red',
@@ -75,18 +75,18 @@ setInterval(() => {
     // red channel-- low byte 0x16, high byte 0x17
     red = colorSensor.readReg(0x97) << 8
     red |= colorSensor.readReg(0x96)
-    red = Math.floor((red * 255) / clear)
+        //red = Math.floor((red * 255) / clear)
     redGraph.update(red)
 
     // green channel-- low byte 0x18, high byte 0x19
     green = colorSensor.readReg(0x99) << 8
     green |= colorSensor.readReg(0x98)
-    green = Math.floor((green * 255) / clear)
+        //green = Math.floor((green * 255) / clear)
     greenGraph.update(green)
 
     // blue channel-- low byte 0x1A, high byte 0x1B
     blue = colorSensor.readReg(0x9B) << 8
     blue |= colorSensor.readReg(0x9A)
-    blue = Math.floor((blue * 255) / clear)
+        //blue = Math.floor((blue * 255) / clear)
     blueGraph.update(blue)
 }, 250)
